d41441ffff245638e3e7ea44a70dbcab255f69af
amapemom-rgb <amapemom@gmail.com>
2025-10-17 19:05:06 +0400

    terraform: Add Pub/Sub module

--- terraform/modules/pubsub/main.tf
+++ terraform/modules/pubsub/main.tf
@@ -1,25 +1,92 @@
-resource "google_pubsub_topic" "topic" {
-  name    = var.topic_name
-  project = var.project_id
+# Pub/Sub Module
+# Creates topics and subscriptions for async messaging
 
-  message_retention_duration = "86400s" # 24 hours
+# Topic for task distribution
+resource "google_pubsub_topic" "tasks_topic" {
+  name = "financial-reports-tasks"
+
+  message_retention_duration = "604800s" # 7 days
+
+  labels = {
+    purpose    = "task-distribution"
+    managed_by = "terraform"
+  }
 }
 
-resource "google_pubsub_subscription" "subscriptions" {
-  for_each = toset(var.subscription_names)
+# Topic for results
+resource "google_pubsub_topic" "results_topic" {
+  name = "financial-reports-results"
 
-  name    = each.value
-  topic   = google_pubsub_topic.topic.name
-  project = var.project_id
+  message_retention_duration = "604800s" # 7 days
 
-  ack_deadline_seconds = 20
+  labels = {
+    purpose    = "results-collection"
+    managed_by = "terraform"
+  }
+}
+
+# Subscription for orchestrator to receive task updates
+resource "google_pubsub_subscription" "orchestrator_tasks_sub" {
+  name  = "orchestrator-tasks-sub"
+  topic = google_pubsub_topic.tasks_topic.name
+
+  ack_deadline_seconds = 60
+
+  message_retention_duration = "604800s"
+  retain_acked_messages      = false
 
   expiration_policy {
-    ttl = "2678400s" # 31 days
+    ttl = "" # Never expire
   }
 
   retry_policy {
     minimum_backoff = "10s"
     maximum_backoff = "600s"
+  }
+}
+
+# Subscription for results aggregation
+resource "google_pubsub_subscription" "results_sub" {
+  name  = "results-aggregation-sub"
+  topic = google_pubsub_topic.results_topic.name
+
+  ack_deadline_seconds = 60
+
+  message_retention_duration = "604800s"
+  retain_acked_messages      = false
+
+  expiration_policy {
+    ttl = "" # Never expire
+  }
+
+  retry_policy {
+    minimum_backoff = "10s"
+    maximum_backoff = "600s"
+  }
+}
+
+# Dead letter topic for failed messages
+resource "google_pubsub_topic" "dead_letter_topic" {
+  name = "financial-reports-dead-letter"
+
+  message_retention_duration = "2678400s" # 31 days
+
+  labels = {
+    purpose    = "dead-letter-queue"
+    managed_by = "terraform"
+  }
+}
+
+# Dead letter subscription
+resource "google_pubsub_subscription" "dead_letter_sub" {
+  name  = "dead-letter-sub"
+  topic = google_pubsub_topic.dead_letter_topic.name
+
+  ack_deadline_seconds       = 600
+  message_retention_duration = "2678400s"
+  retain_acked_messages      = true
+
+  expiration_policy {
+    ttl = "" # Never expire
   }
 }
--- terraform/modules/pubsub/variables.tf
+++ terraform/modules/pubsub/variables.tf
@@ -1,15 +1,6 @@
+# Variables for Pub/Sub module
+
 variable "project_id" {
   description = "GCP Project ID"
   type        = string
-}
-
-variable "topic_name" {
-  description = "Pub/Sub topic name"
-  type        = string
-}
-
-variable "subscription_names" {
-  description = "List of subscription names"
-  type        = list(string)
-  default     = []
 }

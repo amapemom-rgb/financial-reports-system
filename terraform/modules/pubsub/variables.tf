1885100b3de7b0167e19091f9c17baa3bb267229
amapemom-rgb <amapemom@gmail.com>
2025-10-20 12:23:14 +0400

    feat: add orchestrator_url and service_account_email variables to pubsub module

--- terraform/modules/pubsub/variables.tf
+++ terraform/modules/pubsub/variables.tf
@@ -4,3 +4,13 @@ variable "project_id" {
   description = "GCP Project ID"
   type        = string
 }
+
+variable "orchestrator_url" {
+  description = "URL of the orchestrator Cloud Run service"
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "Service account email for Pub/Sub authentication"
+  type        = string
+}
